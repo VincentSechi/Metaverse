ce25467684f9bc452fa6d920ca414047ccec3bc7
VincentSechi <sechivincent@gmail.com>
2023-07-30 13:59:44 +0200

    second commit

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -5,16 +5,16 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import styles from "../styles";
 
-import { staggerContainer } from "../utils/motion"
+import { staggerContainer } from "../utils/motion";
 
 import { TypingText, ExploreCard, TitleText } from "../components";
 
-import { exploreWorlds } from "../constants"
+import { exploreWorlds } from "../constants";
 
 
 const Explore = () => {
 
-  const [active, setActive] = useState('world-2')
+  const [active, setActive] = useState('world-2');
 
   return(
     <section className={`${styles.paddings}`} id="explore">
